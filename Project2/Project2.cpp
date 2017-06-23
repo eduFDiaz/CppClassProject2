d479f6e9212fea341a4e2bed551ed1d07e0207a7
eduFDiaz <erioxcuba@gmail.com>
2017-06-23 07:41:21 -0500

    base code needed

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -1,11 +1,23 @@
-// Project2.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include <ctime> 
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 
+int sign(int x) {
+	return (x > 0) - (x < 0);
+}
 
 int main()
 {
+	srand((unsigned)time(0));
+	int random_integer;
+	int lowest = -10, highest = 10;
+	int range = (highest - lowest) + 1;
+	for (int index = 0; index<20; index++) {
+		random_integer = lowest + int(range*rand() / (RAND_MAX + 1.0));
+		cout << sign(random_integer) << endl;
+	}
     return 0;
 }
 
